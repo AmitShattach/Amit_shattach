d5bcb5255a89e194709c6241d021160dec789bd5
עמית שטח <shattachamit@gmail.com>
2022-09-18 19:37:21 +0300

    my proj

--- web_project_b_id_316389196/Static/Java.js
+++ web_project_b_id_316389196/Static/Java.js
@@ -107,3 +107,74 @@ function enableCreateUser() {
       elements[i].disabled = flag;
     }
   }
+
+
+
+
+
+  function validateInput(){
+    
+    if(email.value.trim()===""){
+        onError(email,"Email cannot be empty");
+        
+    }else{
+        if(!isValidEmail(email.value.trim())){
+            onError(email,"Email is not valid");
+            
+        }else{
+            
+            onSuccess(email);
+        }
+    }
+
+    //password
+    if(password.value.trim()===""){
+        onError(password,"Password cannot be empty");
+ 
+     }else{
+
+         onSuccess(password);
+     }
+}
+
+document.querySelector("button")
+.addEventListener("click",(event)=>{
+    event.preventDefault();
+    validateInput();
+});
+
+function onSuccess(input){
+    let parent=input.parentElement;
+    let messageEle=parent.querySelector("small");
+    messageEle.style.visibility="hidden"; 
+    parent.classList.remove("error");
+    parent.classList.add("success");
+    if(password.value.trim()!=="" && isValidEmail(email.value.trim()))
+    window.location.href = 'Search.html'; 
+}
+function onError(input,message){
+    let parent=input.parentElement;
+    let messageEle=parent.querySelector("small");
+    messageEle.style.visibility="visible";
+    messageEle.innerText=message;  
+    parent.classList.add("error");
+    parent.classList.remove("success");
+
+}
+
+ 
+
+
+function isValidEmail(email){
+   return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+
+
+
+
+
+
+
+
+
